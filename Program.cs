889c88cea86628943a9bb1e1f6a4faab3db60978
Anderson Carneiro da Silva-ANDERC <anderc@motorola.com>
2024-01-07 17:40:39 -0300

    resolvendo desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "4321", modelo: "Modelo 2", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
